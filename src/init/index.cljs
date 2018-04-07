5a46af435aada22b1aa30d348caacf7d31e8f066
jooter <bbb314@gmail.com>
2018-04-08 02:25:40 +1000

    fix autofocus; change color for incorrect answer

--- src/init/index.cljs
+++ src/init/index.cljs
@@ -1,12 +1,9 @@
-(ns main.core
+(ns init.index
   (:refer-clojure :exclude [atom])
   (:require [freactive.core :refer [atom cursor]]
-            [freactive.dom :as dom]
-            #_[chord.client :refer [ws-ch]]
-            #_[cljs.core.async :refer [<! >! put! close!]])
-  (:require-macros #_[cljs.core.async.macros :refer [go]]
-                   [freactive.macros :refer [rx]]))
-    
+            [freactive.dom :as dom])
+  (:require-macros [freactive.macros :refer [rx]]))
+
 (defn log1 [& args]
   (js/console.log (pr-str args)))
 
@@ -18,41 +15,49 @@
 
 (defn view []
   (let [cnt (atom 0)
-        seconds-left (atom 10)
+        seconds-left (atom 20)
         a (atom (rand-int 10)) 
         b (atom (rand-int 10))]
     [:div 
-      [:h1 "Math Super Hero"]
-      ((countdown-component seconds-left))
-      [:p]
+     [:h1 "Math Super Hero"]
+     ((countdown-component seconds-left))
+     [:h1
       (rx (str @a " + " @b " = "))
       [:input {:id "in1"
                :type "text" 
-               :autofocus "autofocus"
-               :style {:width "30%"} 
+               :style {:border "solid #0000ff"} 
                :placeholder "Answer and Enter"
                :on-keypress
-                (fn [e]
-                  (let [v (-> e .-target .-value)
-                        t (-> e .-target)
-                        k (-> e .-key)]
-                     (when (= k "Enter")
-                       (when (= (str (+ @a @b)) v)
+               (fn [e]
+                 (let [v (-> e .-target .-value)
+                       t (-> e .-target)
+                       k (-> e .-key)]
+                   (if (<= @seconds-left 0)
+                     (aset t "disabled" true))
+                   (when (= k "Enter")
+                     (if (not= (str (+ @a @b)) v)
+                       (aset t "style" "border:solid #ff0000")
+                       (do
+                         (aset t "style" "border:solid #0000ff")
                          (reset! a (rand-int 10))
                          (reset! b (rand-int 10))
-                         (if (> @seconds-left 0)
-                           (swap! cnt inc) 
-                           (aset t "disabled" true))
                          (aset t "value" "")
-                         ))))
-              }]
-      [:p "Total correct answers: " (rx (str @cnt))
+                         (swap! cnt inc) 
+                         )))))
+               }]
+      ]
+     [:p "Total correct answers: " (rx (str @cnt))
       ]]))
 
-(defonce root (dom/append-child! (.-body js/document) [:div#root]))
+(set!
+ (.-onload js/window)
+ (fn []
+   (let [root (dom/append-child!
+               (.-body js/document)
+               [:div#root])]
 
-(dom/mount! root (view))
-
-(set! (.-onload js/window)
-  (fn []
-    (log1 "onload")))
+     (aset js/document "title" "Math Hero")
+     (dom/mount! root (view))
+     (log1 "page has been mounted")
+     (.focus (js/document.getElementById "in1"))
+     )))
